fdc975419738ff30ec315a6998ec69af61573198
Phu169 <phu169@gmail.com>
2018-08-06 19:24:54 +0700

    Bai 4 : 04-Sử dụng ngFor directive hiện danh sách một List các object trong Angular 5
    Signed-off-by: Phu169 <phu169@gmail.com>

---
+++ src/app/fake-movies.ts
@@ -0,0 +1,30 @@
+import {Movie} from "../models/movie";
+
+export var fakeMovies: Movie[]=[
+  {
+  id:1,
+  name:"Film 1",
+  releaseYear:2000
+},
+  {
+    id:2,
+    name:"Film 2",
+    releaseYear:2000
+  },
+  {
+    id:3,
+    name:"Film 3",
+    releaseYear:2000
+  },
+  {
+    id:4,
+    name:"Film 4",
+    releaseYear:2000
+  },
+  {
+    id:5,
+    name:"Film 5",
+    releaseYear:2000
+  }
+
+];
